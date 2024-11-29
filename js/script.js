69052550b3ceb295a67a1279edb6826d52dc67b0
Jan Alar Alesmaa <smiletuju@gmail.com>
2024-11-30 00:13:50 +0200

    Final

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function moveSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
